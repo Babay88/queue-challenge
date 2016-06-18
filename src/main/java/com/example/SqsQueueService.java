2d333249f3b4f215c639658ade03121b0cdf4666
adanilov <danilovalexey@mail.ru>
2016-06-19 06:50:18 +1000

    SQS adapter

--- src/main/java/com/example/SqsQueueService.java
+++ src/main/java/com/example/SqsQueueService.java
@@ -1,17 +1,37 @@
 package com.example;
 
 import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
-public class SqsQueueService /* implements QueueService */ {
-	//
-	// Task 4: Optionally implement parts of me.
-	//
-	// This file is a placeholder for an AWS-backed implementation of QueueService. It is included
-	// primarily so you can quickly assess your choices for method signatures in QueueService in
-	// terms of how well they map to the implementation intended for a production environment.
-	//
+public class SqsQueueService implements QueueService {
+
+	private AmazonSQSClient sqs;
+
+	private String TEMPORARY_DEFAULT_URL = "default";
 
 	public SqsQueueService(AmazonSQSClient sqsClient) {
+		this.sqs = sqsClient;
 	}
 
+	@Override
+	public void push(String messageBody) {
+		sqs.sendMessage(new SendMessageRequest(TEMPORARY_DEFAULT_URL, messageBody));
+	}
+
+	@Override
+	public Message pull() {
+		ReceiveMessageResult sqsMsg = sqs.receiveMessage(TEMPORARY_DEFAULT_URL);
+		com.amazonaws.services.sqs.model.Message message = sqsMsg.getMessages().get(0);// TODO
+
+		String messageBody = message.getBody();
+		String receiptHandle = message.getReceiptHandle();
+
+		return new Message(messageBody, receiptHandle);
+	}
+
+	@Override
+	public void delete(String receiptHandle) {
+		sqs.deleteMessage(TEMPORARY_DEFAULT_URL, receiptHandle);
+	}
 }
