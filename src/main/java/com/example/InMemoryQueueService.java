4b0bd86c7c488b1ece062d0dba4b37ae49c30f46
adanilov <danilovalexey@mail.ru>
2016-06-19 06:27:30 +1000

    visibility timeout implemented

--- src/main/java/com/example/InMemoryQueueService.java
+++ src/main/java/com/example/InMemoryQueueService.java
@@ -1,15 +1,22 @@
 package com.example;
 
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 public class InMemoryQueueService implements QueueService {
 
+	private final static int VISIBILITY_TIMEOUT_MILLIS = 100;
+
 	private Queue<String> q = new LinkedList<>();
 	private Map<String, Message> invisibleMessages = new HashMap<String, Message>();
+	private Map<String, TimerTask> invisibleMessageRemovalTasks = new HashMap<String, TimerTask>();
+	private Timer timer = new Timer();
 
 	@Override
 	public void push(String messageBody) {
@@ -22,11 +29,28 @@ public class InMemoryQueueService implements QueueService {
 		String receiptHandle = UUID.randomUUID().toString();
 		Message message = new Message(messageBody, receiptHandle);
 		invisibleMessages.put(receiptHandle, message);
+
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				Deque<String> d = (Deque<String>) q;
+				d.addFirst(messageBody);
+
+				invisibleMessages.remove(receiptHandle);
+				invisibleMessageRemovalTasks.remove(receiptHandle);
+			}
+
+		}, VISIBILITY_TIMEOUT_MILLIS);
+
 		return message;
 	}
 
 	@Override
 	public void delete(String receiptHandle) {
+		invisibleMessageRemovalTasks.get(receiptHandle).cancel();
+
 		invisibleMessages.remove(receiptHandle);
+		invisibleMessageRemovalTasks.remove(receiptHandle);
 	}
 }
--- src/test/java/com/example/InMemoryQueueTest.java
+++ src/test/java/com/example/InMemoryQueueTest.java
@@ -8,13 +8,32 @@ public class InMemoryQueueTest {
 	@Test
 	public void testOneThread() {
 		QueueService qs = new InMemoryQueueService();
-		Assert.assertTrue(qs != null);
+
 		qs.push("TEST_MSG_1");
 		qs.push("TEST_MSG_2");
 
-		Message message1 = qs.pull();
-		Assert.assertTrue(message1.getBody().equals("TEST_MSG_1"));
-		Message message2 = qs.pull();
-		Assert.assertTrue(message2.getBody().equals("TEST_MSG_2"));
+		Message message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+
+		message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_2"));
+	}
+
+	@Test
+	public void testOneThreadWithDelay() throws InterruptedException {
+		QueueService qs = new InMemoryQueueService();
+
+		qs.push("TEST_MSG_1");
+		qs.push("TEST_MSG_2");
+
+		Message message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+
+		Thread.sleep(110);
+
+		message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+		message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_2"));
 	}
 }
