cec1027ba5ec7bf87d243de05da97374e7b8a624
adanilov <danilovalexey@mail.ru>
2016-06-19 13:45:59 +1000

    improved tests

--- src/test/java/com/example/InMemoryQueueTest.java
+++ src/test/java/com/example/InMemoryQueueTest.java
@@ -14,26 +14,55 @@ public class InMemoryQueueTest {
 
 		Message message = qs.pull();
 		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+		qs.delete(message.getReceiptHandle());
 
 		message = qs.pull();
 		Assert.assertTrue(message.getBody().equals("TEST_MSG_2"));
+		qs.delete(message.getReceiptHandle());
 	}
 
 	@Test
 	public void testOneThreadWithDelay() throws InterruptedException {
-		QueueService qs = new InMemoryQueueService();
+		QueueService qs = new InMemoryQueueService();//TODO 100
 
 		qs.push("TEST_MSG_1");
 		qs.push("TEST_MSG_2");
 
 		Message message = qs.pull();
 		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+		String receiptHandleFirst = message.getReceiptHandle();
 
 		Thread.sleep(110);
 
 		message = qs.pull();
+		String receiptHandleSecond = message.getReceiptHandle();
 		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+
+		Assert.assertFalse(receiptHandleFirst.equals(receiptHandleSecond));
+
 		message = qs.pull();
 		Assert.assertTrue(message.getBody().equals("TEST_MSG_2"));
+	}
+
+	@Test
+	public void testDelete() throws InterruptedException {
+		QueueService qs = new InMemoryQueueService();//TODO 100
+
+		qs.push("TEST_MSG_1");
+		qs.push("TEST_MSG_2");
+
+		Message message = qs.pull();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_1"));
+		String receiptHandleFirst = message.getReceiptHandle();
+		qs.delete(receiptHandleFirst);
+
+		message = qs.pull();
+		String receiptHandleSecond = message.getReceiptHandle();
+		Assert.assertTrue(message.getBody().equals("TEST_MSG_2"));
+
+		Assert.assertFalse(receiptHandleFirst.equals(receiptHandleSecond));
+
+		Thread.sleep(200);
+		qs.delete(receiptHandleSecond);
 	}
 }
